7b03a320f9903a3fe698d8e7e36f34c9f522a2ea
Little Jacob <joelnoumbi28@gmail.com>
2023-04-25 20:28:58 +0100

    --comeback later

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,10 +1,15 @@
 import React, { createContext, useContext } from "react";
 
-const AuthContext = createContext({});
+export const AuthContext = createContext({
+  user: "",
+});
 
 export const AuthProvider = ({ children }: { children: React.ReactNode }) => {
+  const value = {
+    user: "value",
+  };
   return (
-    <AuthContext.Provider value={{ user: "Joel" }}>
+    <AuthContext.Provider value={value}>
       {children}
     </AuthContext.Provider>
   );
